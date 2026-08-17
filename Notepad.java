e6789f85d1c8c5a3de65f1687fbe3781e6d55f55
FrostyDanube <300347623+FrostyDanube@users.noreply.github.com>
2026-08-16 18:31:37 -0600

    Updated Notepad.java
    removed the copyright notice

--- Notepad.java
+++ Notepad.java
@@ -1,10 +1,6 @@
 /**
  * Copyright FrostyDanube - All Rights Reserved.
  * DanubePad 1.2
- * ----------------------------------------
- * This program is free software, you can distribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as published by
- * the Free Software Foundation.
  */
 
 import java.awt.*;
